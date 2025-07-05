c02d0b26bc59038f29a4dff7064019067ed49a22
Wafiul Achdi <achdiwafiul@gmail.com>
2025-07-05 23:19:36 +0700

    chore: set up project structure and create application header component

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import Search from './components/Search'
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  return (
+    <main>
+      <div className='pattern'/>
+      <div className='wrapper'>
+        <header>
+        <img src="./hero.png" alt="Hero Banner"/>
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        <h1 className='text-white'>{searchTerm}</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
