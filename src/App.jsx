fc0375104d5b24dfa3bbb5c18edf26da15c35221
Wafiul Achdi <achdiwafiul@gmail.com>
2025-07-06 23:36:55 +0700

    feat(appwrite): set up backend for trending movie feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from './components/Search';
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
+import { updateSearchCount } from './appWrite';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -46,6 +47,9 @@ const App = () => {
       }
 
       setMovieList(data.results || []);
+      if(query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       console.log(`Error fetching movies: ${error}`);
       setErrorMessage('Error fetching movies. Please try again later.');
